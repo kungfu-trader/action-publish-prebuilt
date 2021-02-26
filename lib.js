98103a4d8d2425c7ccb01dfbcb4fb701f409ab1e
Keren Dong <dongkeren@gmail.com>
2021-02-26 17:40:20 +0800

    Get trace on action runner

--- lib.js
+++ lib.js
@@ -1,5 +1,14 @@
 const path = require("path");
 const glob = require("glob");
+const { spawnSync } = require("child_process");
+
+const spawnOpts = { shell: true, stdio: "inherit", windowsHide: true };
 
 exports.publish = function (artifactsPath) {
+  console.log(`cwd: ${process.cwd()}`);
+  glob.sync(path.join(artifactsPath, "*")).map(p => {
+    console.log(`artifact: ${p}`);
+  });
+  spawnSync("aws", ["s3", "ls", "kungfu/core/v2/2.3.0/"], spawnOpts);
+  spawnSync("cat", ["/etc/hosts"], spawnOpts);
 };
