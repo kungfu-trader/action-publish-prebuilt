bd687d36f2bf95639b51ba201f2f44e19856d0c4
Keren Dong <dongkeren@gmail.com>
2021-02-26 18:05:15 +0800

    Add more trace

--- lib.js
+++ lib.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 const glob = require("glob");
 const { spawnSync } = require("child_process");
@@ -7,8 +8,15 @@ const spawnOpts = { shell: true, stdio: "inherit", windowsHide: true };
 exports.publish = function (artifactsPath) {
   console.log(`cwd: ${process.cwd()}`);
   glob.sync(path.join(artifactsPath, "*")).map(p => {
-    console.log(`artifact: ${p}`);
+    const stat = fs.lstatSync(p);
+    console.log(`artifact: ${p}, directory: ${stat.isDirectory()}`);
+    console.log(stat);
+    console.log('---');
   });
   spawnSync("aws", ["s3", "ls", "kungfu/core/v2/2.3.0/"], spawnOpts);
+  console.log('---');
+  spawnSync("cat", ["/etc/hosts"], spawnOpts);
+  console.log('---');
+  fs.appendFileSync('/etc/hosts', '127.0.0.1 local-test-kungfu');
   spawnSync("cat", ["/etc/hosts"], spawnOpts);
 };
