ccfd58ce234afd1630bd05739eb2d4da7041f34c
Keren Dong <dongkeren@gmail.com>
2021-03-01 11:26:40 +0800

    List artfacts content

--- lib.js
+++ lib.js
@@ -8,15 +8,16 @@ const spawnOpts = { shell: true, stdio: "inherit", windowsHide: true };
 exports.publish = function (artifactsPath) {
   console.log(`cwd: ${process.cwd()}`);
   glob.sync(path.join(artifactsPath, "*")).map(p => {
-    const stat = fs.lstatSync(p);
-    console.log(`artifact: ${p}, directory: ${stat.isDirectory()}`);
-    console.log(stat);
-    console.log('---');
+    console.log(`artifact: ${p}`);
+    fs.readdirSync(p).forEach(file => {
+      const stat = fs.lstatSync(p);
+      console.log(`\t${file} size=${stat.size}`);
+    });
   });
-  spawnSync("aws", ["s3", "ls", "kungfu/core/v2/2.3.0/"], spawnOpts);
   console.log('---');
-  spawnSync("cat", ["/etc/hosts"], spawnOpts);
+  spawnSync("aws", ["s3", "ls", "kungfu/core/v2/2.3.0/"], spawnOpts);
   console.log('---');
   fs.appendFileSync('/etc/hosts', '127.0.0.1 local-test-kungfu');
   spawnSync("cat", ["/etc/hosts"], spawnOpts);
+  spawnSync("traceroute", ["kungfu.s3.cn-northwest-1.amazonaws.com.cn"], spawnOpts);
 };
