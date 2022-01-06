b9cd6bff114e2c795de2fbec8f1e1eb6c55a6549
Keren DONG <keren.dong@kungfu.link>
2022-01-06 16:23:05 +0800

    Add max preview links option

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const main = async function () {
   const bucketRelease = core.getInput('bucket-release');
   const withDigest = core.getInput('no-digest') === 'false';
   const withComment = core.getInput('no-comment') === 'false';
+  const maxPreviewLinks = parseInt(core.getInput('max-preview-links'), 10) || 1;
   const repo = github.context.repo;
   const pullRequestNumber = () => (context.issue.number ? context.issue.number : context.payload.pull_request.number);
 
